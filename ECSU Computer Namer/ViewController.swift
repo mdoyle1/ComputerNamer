75eb73d8ccc9ab37cdb0a96de261a7d6d7183376
Doyle <doylema@eitsackgg7v.easternct.edu>
2018-12-04 15:54:54 -0500

    ECSU Computer Namer

--- ECSU Computer Namer/ViewController.swift
+++ ECSU Computer Namer/ViewController.swift
@@ -8,12 +8,101 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
 
+//Serial #
+let platformExpert = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice") )
+let serialNumber = (IORegistryEntryCreateCFProperty(platformExpert, kIOPlatformSerialNumberKey as CFString, kCFAllocatorDefault, 0).takeUnretainedValue()).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+//Eastern Naming Catagories
+
+var facultyStaff = "E"
+var studentWorker = "EDF"
+var lab = "ELAB"
+var other = ""
+var setPre = "E"
+
+//Departments
+var departmentTable = [
+    "Accessability": "ACCS",
+    "Accounting": "ACCT",
+    "Academic Affairs": "ADAF",
+    "Admissions": "ADMN",
+    "Advising Center": "ADVC",
+    "School of Arts & Sciences": "ASCI",
+    "Athletics": "ATHL",
+    "Biology": "BIOL",
+    "Book Store": "BOOK",
+    "Business Administration": "BUAD",
+    "Card Services": "CARD",
+    "Media Carts": "CART",
+    "Center Early Childhood Educ": "CECE",
+    "School of Continuing Education": "CEDU",
+    "Child & Family Development Ctr": "CFDC",
+    "Communication": "COMM",
+    "Counseling & Psycholocial Svcs": "COUN",
+    "Economics": "ECON",
+    "Education": "EDUC",
+    "English": "ENGL",
+    "Environmental Earth Science": "ENVS",
+    "School of Educ & Prof Studies": "EPRF"]
+
+class ViewController: NSViewController {
+    
+    //Checkboxes
+    @IBOutlet weak var chkFacultyStaff: NSButton!
+    @IBOutlet weak var chkStudentWorker: NSButton!
+    @IBOutlet weak var chkLab: NSButton!
+    @IBOutlet weak var chkOther: NSButton!
+    
+    //Labels
+    @IBOutlet weak var buildingLabel: NSTextField!
+    @IBOutlet weak var departmentLBL: NSTextField!
+    
+    //Pop-up Menus
+    
+    @IBOutlet weak var departPopUp: NSPopUpButton!
+    
+    //Actions
+    
+    
+    //Faculty Staff
+    @IBAction func facultyStaffFunc(_ sender: NSButton) {
+        
+        setPre = "E"
+        
+        if chkFacultyStaff.state == NSControl.StateValue(rawValue: 1){
+            //Show
+            departmentLBL.isHidden = false
+            departPopUp.isHidden = false
+            
+            //Hide
+            buildingLabel.isHidden = true
+            
+        } else {
+            buildingLabel.isHidden = false
+        }
+        
+        
+    }
+    
+    //Student Worker
+    @IBAction func studentWorkFunc(_ sender: NSButton) {
+        
+        setPre = "EDF"
+        
+        if chkStudentWorker.state == NSControl.StateValue(rawValue: 1){
+            departPopUp.isHidden = false
+            departmentLBL.isHidden = false
+            chkFacultyStaff.state = NSControl.StateValue(rawValue: 0)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override var representedObject: Any? {
@@ -21,6 +110,21 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    @IBOutlet weak var departOptions: NSPopUpButton!
+    @IBOutlet weak var computerName: NSTextField!
+    
+    @IBAction func departSelect(_ sender: NSPopUpButton) {
+        var department = departOptions.titleOfSelectedItem
+        let prefix = departmentTable[departOptions.titleOfSelectedItem!]
+        let serialEnd = serialNumber.endIndex
+        let serialStart = serialNumber.index(serialEnd, offsetBy: -6)
+        let range = Range(uncheckedBounds: (lower: serialStart, upper: serialEnd))
+        let lastSix = serialNumber[range]
+        computerName.stringValue = (setPre+prefix!+lastSix ?? nil)!
+        print (prefix)
+    }
+    
 
 
 }
