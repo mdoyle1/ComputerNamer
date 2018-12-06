985f1e7a2015f087415f0b2e2724dab7c62f7a4d
Doyle <doylema@eitsackgg7v.easternct.edu>
2018-12-06 09:02:23 -0500

    Working on setting up the departments Array...

--- ECSU Computer Namer/ViewController.swift
+++ ECSU Computer Namer/ViewController.swift
@@ -22,6 +22,7 @@ var other = ""
 var setPre = "E"
 
 //Departments
+
 var departmentTable = [
     "Accessability": "ACCS",
     "Accounting": "ACCT",
@@ -44,8 +45,50 @@ var departmentTable = [
     "Education": "EDUC",
     "English": "ENGL",
     "Environmental Earth Science": "ENVS",
-    "School of Educ & Prof Studies": "EPRF"]
-
+    "School of Educ & Prof Studies": "EPRF",
+    "Financial Aid": "FAID",
+    "Facilities": "FCIL",
+    "Fine Arts": "FIAR",
+    "Food Service": "FOOD",
+    "Fiscal Affairs": "FSCL",
+    "Graduate School": "GRAD",
+    "History": "HIST",
+    "Kinesiology and Physical Educ": "HLPE",
+    "Honors Department": "HONR",
+    "Housing":    "HOUS",
+    "Human Resources ": "HRES",
+    "Health Sciences": "HSCI",
+    "Health Services": "HSVS",
+    "Institutional Advancement": "IADV",
+    "Intercultural": "ICCT",
+    "Institute for Sustainable Energy": "IFSE",
+    "Institutional Research": "IRES",
+    "ITS- (other than Banner Team)": "ITSA",
+    "Info Tech Services - Banner Team": "ITSB",
+    "Judicial": "JDSV",
+    "Lecterns": "LECT",
+    "Learning Center ": "LERN",
+    "Library": "LIBR",
+    "Mathematics": "MATH",
+    "Media Services": "MSVC",
+    "Performing Arts": "PART",
+    "Professional Development": "PDEV",
+    "Physical Science": "PHSC",
+    "Police Dept.": "PLDP",
+    "PoliSci, Philosophy, & Geog": "POLI",
+    "Presidents Office": "PRES",
+    "Psychological Science": "PSYC",
+    "Registrar Office": "REGS",
+    "ROTC, UConn": "ROTC",
+    "Student Activities": "SACT",
+    "Sociology, Anthro & Social Wk": "SOCI",
+    "Student Affairs": "STAF",
+    "University Relations": "UNRL",
+    "Art and Art History": "VART",
+    "Veterans Affairs": "VETN",
+    "Womens Center": "WNCT",
+    "World Language and Cultures": "WOLC"
+]
 class ViewController: NSViewController {
     
     //Checkboxes
@@ -102,6 +145,16 @@ class ViewController: NSViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //Total # of Departments
+        let departmentCount = departmentTable.count
+        
+        //Set Departments to an array
+        let departmentKeys = Array(departmentTable.keys)
+       //Sort the Array in alphabetical order
+        let departSorted = departmentKeys.sorted { $0 < $1 }
+        
+        print(departSorted[0])
+        print(departmentCount)
         
     }
 
@@ -115,9 +168,15 @@ class ViewController: NSViewController {
     
     //Department Selection Pop Up Button
     @IBOutlet weak var departOptions: NSPopUpButton!
+   
+   
+    
     @IBOutlet weak var computerName: NSTextField!
     
     @IBAction func departSelect(_ sender: NSPopUpButton) {
+        
+        departOptions.addItems(withTitles: ["\(departmentTable)"])
+        
         var department = departOptions.titleOfSelectedItem
         let prefix = departmentTable[departOptions.titleOfSelectedItem!]
         let serialEnd = serialNumber.endIndex
---
+++ departmentTable.swift
@@ -0,0 +1,37 @@
+//
+//  departmentTable.swift
+//  ECSU Computer Namer
+//
+//  Created by Doyle, Mark(Information Technology Services) on 12/5/18.
+//  Copyright © 2018 Doyle, Mark(Information Technology Services). All rights reserved.
+//
+
+import Cocoa
+
+
+
+class departmentTable: NSObject {
+var names = [
+        "Accessability": "ACCS",
+        "Accounting": "ACCT",
+        "Academic Affairs": "ADAF",
+        "Admissions": "ADMN",
+        "Advising Center": "ADVC",
+        "School of Arts & Sciences": "ASCI",
+        "Athletics": "ATHL",
+        "Biology": "BIOL",
+        "Book Store": "BOOK",
+        "Business Administration": "BUAD",
+        "Card Services": "CARD",
+        "Media Carts": "CART",
+        "Center Early Childhood Educ": "CECE",
+        "School of Continuing Education": "CEDU",
+        "Child & Family Development Ctr": "CFDC",
+        "Communication": "COMM",
+        "Counseling & Psycholocial Svcs": "COUN",
+        "Economics": "ECON",
+        "Education": "EDUC",
+        "English": "ENGL",
+        "Environmental Earth Science": "ENVS",
+        "School of Educ & Prof Studies": "EPRF"]
+}
